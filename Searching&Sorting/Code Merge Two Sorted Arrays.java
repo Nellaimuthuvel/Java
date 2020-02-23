a9f4a26270cf0417f7d4025b98df2dd67d37473a
Nellaimuthuvel S <34041597+Nellaimuthuvel@users.noreply.github.com>
2020-02-23 23:59:59 +0530

    Code Merge Two Sorted Arrays

---
+++ Searching&Sorting/Code Merge Two Sorted Arrays.java
@@ -0,0 +1,43 @@
+
+public class Solution {
+
+	public static int[] merge(int arr1[], int arr2[]){
+		
+		/* Your class should be named Solution
+	 	* Don't write main().
+	 	* Don't read input, it is passed as function argument.
+	 	* Return output and don't print it.
+		* Taking input and printing output is handled automatically.
+		*/	
+        int n1= arr1.length;
+        int n2= arr2.length;
+        int[] arr3 = new int[n1+n2]; 
+       int i = 0, j = 0, k = 0; 
+      
+        // Traverse both array 
+        while (i<n1 && j <n2) 
+        { 
+            // Check if current element of first 
+            // array is smaller than current element 
+            // of second array. If yes, store first 
+            // array element and increment first array 
+            // index. Otherwise do same with second array 
+            if (arr1[i] < arr2[j]) 
+                arr3[k++] = arr1[i++]; 
+            else
+                arr3[k++] = arr2[j++]; 
+        } 
+      
+        // Store remaining elements of first array 
+        while (i < n1) 
+            arr3[k++] = arr1[i++]; 
+      
+        // Store remaining elements of second array 
+        while (j < n2) 
+            arr3[k++] = arr2[j++];  
+            return arr3;
+
+
+	}
+	
+}
