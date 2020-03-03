09566bd5ffb9167da7838c8cedfaf96ba786e8bc
Nellaimuthuvel S <34041597+Nellaimuthuvel@users.noreply.github.com>
2020-03-04 02:17:47 +0530

    ArrayRotationCheck

---
+++ Recursion/CheckArrayRotation.java
@@ -0,0 +1,27 @@
+public class CheckRotation {	
+
+	public static int arrayRotateCheck(int[] arr){
+		/* Your class should be named CheckRotation
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+		 * Taking input and printing output is handled automatically.
+		 */
+
+
+int n = arr.length;
+    int min = arr[0], min_index = -1; 
+        
+        for (int i = 0; i < n; i++) 
+        { 
+            if (min > arr[i]) 
+            { 
+                min = arr[i]; 
+                min_index = i; 
+            } 
+        }  
+                    return min_index; 
+
+    }
+
+}
